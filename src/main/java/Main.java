0810738bdeb43b53b350a036c9e479e7008b167f
Harsh-Choudhary-01 <harshch000@gmail.com>
2016-12-16 12:43:57 -0600

    Initialized files

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.get;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    port(Integer.valueOf(System.getenv("PORT")));
+    staticFileLocation("/public");
+
+    get("/hello", (req, res) -> "Hello World");
+
+    get("/", (request, response) -> {
+            Map<String, Object> attributes = new HashMap<>();
+            attributes.put("message", "Hello World!");
+
+            return new ModelAndView(attributes, "index.ftl");
+        }, new FreeMarkerEngine());
+
+
+  }
+
+}
