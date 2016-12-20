f5dc31fa68b18391d187aace989612ef93b10590
Harsh-Choudhary-01 <harshch000@gmail.com>
2016-12-20 11:46:32 -0600

    SQL Class query

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,76 @@
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Base64;
-
 import com.auth0.jwt.JWTVerifier;
 
 import static spark.Spark.*;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import spark.Request;
 import spark.template.freemarker.FreeMarkerEngine;
 import spark.ModelAndView;
-
 import static spark.Spark.get;
-
+import java.security.SecureRandom;
+import java.math.BigInteger;
 import com.heroku.sdk.jdbc.DatabaseUrl;
 
 public class Main {
-    static String clientId = System.getenv("AUTH0_CLIENT_ID");
-    static String clientDomain = System.getenv("AUTH0_DOMAIN");
-
+    private static String clientId = System.getenv("AUTH0_CLIENT_ID");
+    private static String clientDomain = System.getenv("AUTH0_DOMAIN");
+    private static String managementToken = System.getenv("AUTH0_MANAGE");
     public static void main(String[] args) {
 
         port(Integer.valueOf(System.getenv("PORT")));
         staticFileLocation("/spark/template/freemarker");
-
-        get("/hello", (req, res) -> "Hello World");
+        SecureRandom random = new SecureRandom();
+        get("/hello", (request, response) -> "Hello World");
 
         get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             Map<String , Object> user = getUser(request);
             attributes.put("loggedIn" , user.get("loggedIn"));
-            if((Boolean) attributes.get("loggedIn"))
-                attributes.put("user" , user.get("claims"));
+            if((Boolean) attributes.get("loggedIn")) {
+                attributes.put("user", user.get("claims"));
+                Map<String , Object> metadata = getDynamicUser((String)((Map<String , Object>)attributes.get("user")).get("user_id"));
+                if(metadata != null)
+                {
+                    Connection connection = null;
+                    try {
+                        if (((Map<String, Object>) metadata.get("app_metadata")).get("role") == "teacher") {
+                            connection = DatabaseUrl.extract().getConnection();
+                            Statement stmt = connection.createStatement();
+                            ResultSet rs = stmt.executeQuery("SELECT className , classID , cardinality(assignments) AS assignLength , cardinality(joinedStudents) AS joinedLength FROM classes WHERE ownerID = '" + ((Map<String, Object>)attributes.get("user")).get("user_id") + "'");
+                            //loop through classes and get info for page, construct class map to pass to ftl, rmbr to test for null exceptions
+                            ArrayList<Object> classes = new ArrayList<>();
+                            Map<String, Object> classObject = new HashMap<>();
+                            while(rs.next())
+                            {
+                                classObject.clear();
+                                classObject.put("name" , rs.getString(1)); //className
+                                classObject.put("classID" , rs.getString(2)); //classID
+                                classObject.put("numAssignments" , rs.getInt("assignLength")); //num Assignments
+                                classObject.put("numJoined" , rs.getInt("joinedStudents")); //num Joined
+                                classes.add(classObject);
+                            }
+                            attributes.put("classes" , classes);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        System.out.println(e);
+                    }
+                    finally {
+                        if(connection != null) try { connection.close(); } catch(SQLException e) {}
+                    }
+                }
+                attributes.put("metadata" , metadata);
+            }
             attributes.put("message", "Hello World!");
             attributes.put("clientId", clientId);
             attributes.put("clientDomain", clientDomain);
@@ -48,9 +87,59 @@ public class Main {
             response.redirect("/");
         });
 
+        before("/update" , (request , response) -> {
+            Map<String , Object> user = getUser(request);
+
+            if((Boolean) user.get("loggedIn")) {
+                user = (Map<String, Object>) user.get("claims");
+                String role = request.queryParams("role");
+                if(role != null && (role.equals("student") || role.equals("teacher"))) {
+                    Unirest.patch("https://" + clientDomain + "/api/v2/users/" + user.get("user_id"))
+                            .header("authorization", "Bearer " + managementToken)
+                            .header("content-type", "application/json")
+                            .body("{\"app_metadata\" : { \"role\" : \"" + role + "\" } }")
+                            .asString();
+                }
+            }
+           response.redirect("/");
+        });
+
+        before("/newclass" , (request, response) -> { //TODO: check to make sure role is teacher
+            String classID = new BigInteger(30, random).toString(32);
+            Map<String, Object> user = getUser(request);
+            if((Boolean) user.get("loggedIn")) {
+                Map<String, Object> claims = (Map<String, Object>) user.get("claims");
+                HttpResponse<JsonNode> appMetadata = Unirest.get("https://" + clientDomain + "/api/v2/users/" + claims.get("user_id") + "?fields=app_metadata")
+                        .header("authorization", "Bearer " + managementToken)
+                        .asJson();
+                String classArray = appMetadata.getBody().getObject().getJSONObject("app_metadata").getJSONArray("classes").put(classID).toString();
+                Unirest.patch("https://" + clientDomain + "/api/v2/users/" + claims.get("user_id"))
+                        .header("authorization", "Bearer " + managementToken)
+                        .header("content-type", "application/json")
+                        .body("{\"app_metadata\" : { \"classes\" : " + classArray + "} }")
+                        .asString();
+                Connection connection = null;
+                try
+                {
+                    connection = DatabaseUrl.extract().getConnection();
+                    Statement stmt = connection.createStatement();
+                    stmt.executeUpdate("CREATE TABLE IF NOT EXISTS classes (classID text , className text, ownerID text , assignments text[] DEFAULT '{}', joinedStudents text[] DEFAULT '{}' , invitedStudents text[] DEFAULT '{}')");
+                    stmt.executeUpdate("INSERT INTO classes(classID , className, ownerID) VALUES ('" + classID + "' , 'New Class' , '" + claims.get("user_id") + "')");
+                }
+                catch (Exception e)
+                {
+                    System.out.println("Exception" + e.toString());
+                }
+                finally {
+                    if(connection != null) try { connection.close(); } catch(SQLException e) {}
+                }
+            }
+            response.redirect("/?t=one");
+        });
+
     }
 
-    static Map<String, Object> getUser(Request request) {
+    private static Map<String, Object> getUser(Request request) { //Returns a map with a loggedIn value. If the loggedIn value is true also contains a value with key "claims"
         Map<String, Object> user = new HashMap<>();
         Map<String, Object> userInfo;
         if (request.session().attribute("token") == null) {
@@ -58,16 +147,15 @@ public class Main {
         } else {
             String token = request.session().attribute("token");
             userInfo = checkToken(token);
-            if (userInfo.containsKey("loggedIn")) {
-                request.session().attribute("token", token);
+            if (userInfo.containsKey("loggedIn"))
                 user = userInfo;
-            } else
+            else
                 user.put("loggedIn", false);
         }
         return user;
     }
 
-    static Map<String, Object> checkToken(String token) {
+    private static Map<String, Object> checkToken(String token) {
         Map<String, Object> values = new HashMap<>();
         final String secret = System.getenv("AUTH0_CLIENT_SECRET");
         final byte[] decodedSecret = Base64.getUrlDecoder().decode(secret);
@@ -82,5 +170,24 @@ public class Main {
             return values;
         }
     }
+
+    private static Map<String, Object> getDynamicUser(String userID) //returns dynamic user info like metadata
+    {
+        try {
+            HttpResponse<String> response = Unirest.get("https://" + clientDomain + "/api/v2/users/" + userID + "?fields=app_metadata")
+                    .header("Authorization", "Bearer " + managementToken)
+                    .asString();
+            String json = response.getBody();
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(json, new TypeReference<Map<String , Object>>(){});
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+            System.out.println("Exception getting dynamic user");
+            return null;
+        }
+    }
+
 
 }
--- src/main/resources/spark/template/freemarker/index.ftl
+++ src/main/resources/spark/template/freemarker/index.ftl
@@ -6,6 +6,9 @@
 -->
 <html>
 <#assign verified = loggedIn>
+<#if loggedIn>
+	<#assign role = (metadata.app_metadata.role)!"no_role">
+</#if>
 	<head>
 		<title>PESH CS</title>
 		<meta charset="utf-8" />
@@ -14,7 +17,6 @@
 		<link rel="stylesheet" href="css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie8.css" /><![endif]-->
-	</head>
 	<body>
 
 		<!-- Sidebar -->
@@ -24,8 +26,14 @@
 						<ul>
 							<#if verified>
 								<li><a href="#intro">Welcome ${user.nickname}</a></li>
-								<li><a href="#one">Assignments</a></li>
-								<li><a href="/logout">Logout</a></li>
+								<#if role != "no_role">
+									<#if role == "student">
+										<li><a href="#one">Assignments</a></li>
+									<#else>
+										<li><a href="#one">Classes</a></li>
+									</#if>
+								</#if>
+								<li><a href="logout">Logout</a></li>
 							<#else>
 								<li><a href="#intro">Welcome</a></li>
 							</#if>
@@ -54,44 +62,78 @@
 
 				<!-- One -->
 					<#if verified>
-						<section id="one" class="wrapper style2 spotlights">
-							<section>
-								<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
-								<div class="content">
-									<div class="inner">
-										<h2>Sed ipsum dolor</h2>
-										<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-										<ul class="actions">
-											<li><a href="#" class="button">Learn more</a></li>
-										</ul>
-									</div>
+						<#if role == "no_role">
+							<section id="role" class="wrapper style2 roleChoose">
+								<div class="inner" style="text-align: center; margin: auto;">
+									<h2>What is your role:</h2>
+									<ul class="actions vertical">
+										<li><a href="#" class="button special student">Student</a></li>
+										<li><a href="#" class="button special teacher">Teacher</a></li>
+									</ul>
 								</div>
 							</section>
-							<section>
-								<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
-								<div class="content">
-									<div class="inner">
-										<h2>Feugiat consequat</h2>
-										<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-										<ul class="actions">
-											<li><a href="#" class="button">Learn more</a></li>
-										</ul>
-									</div>
-								</div>
+						<#else>
+							<section id="one" class="wrapper style2 spotlights">
+								<#if role == "teacher">
+									<#list classes as class>
+										<section>
+											<#assign s = ("images/pic0" + (class?counter % 2 + 1) + ".jpg")>
+											<img src="${s}" alt="" data-position="center center"/>
+											<div class="content">
+												<div class="inner">
+													<h2>${class.name}</h2>
+													<p>Number of Assignments: ${class.numAssignments}<br>Number of Joined Students: ${class.numJoined}</p>
+													<ul class="actions">
+														<li><a href="#" class="button">Edit Class</a></li>
+													</ul>
+												</div>
+											</div>
+										</section>
+									</#list>
+									<section>
+										<img src="images/pic02.jpg" alt="" data-position="center center"/>
+										<div class="content">
+											<div class="inner">
+												<#if classes?size == 0>
+													<h2>No Classes</h2>
+												<#else>
+													<h2>Create New Class</h2>
+												</#if>
+												<p>Create a new class by using the appropiate button below.</p>
+												<ul class="actions">
+													<li><a href="/newclass" class="button">Create Class</a></li>
+												</ul>
+											</div>
+										</div>
+									</section>
+								<#else>
+									<section>
+										<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+										<div class="content">
+											<div class="inner">
+												<h2>Feugiat consequat</h2>
+												<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+												<ul class="actions">
+													<li><a href="#" class="button">Learn more</a></li>
+												</ul>
+											</div>
+										</div>
+									</section>
+									<section>
+										<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+										<div class="content">
+											<div class="inner">
+												<h2>Ultricies aliquam</h2>
+												<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
+												<ul class="actions">
+													<li><a href="#" class="button">Learn more</a></li>
+												</ul>
+											</div>
+										</div>
+									</section>
+								</#if>
 							</section>
-							<section>
-								<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
-								<div class="content">
-									<div class="inner">
-										<h2>Ultricies aliquam</h2>
-										<p>Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus.</p>
-										<ul class="actions">
-											<li><a href="#" class="button">Learn more</a></li>
-										</ul>
-									</div>
-								</div>
-							</section>
-						</section>
+						</#if>
 					</#if>
 		<!-- Footer -->
 			<footer id="footer" class="wrapper style1-alt">
@@ -112,15 +154,27 @@
 			<script src="js/main.js"></script>
 			<script src="https://cdn.auth0.com/js/lock/10.0/lock.min.js"></script>
 		    <script>
+				if(${(loggedIn && role == "no_role")?c})
+					window.location.href="#role";
 		      $(document).ready(function()
 		      {
+		      	$(".student").click(function(e)
+		      	{
+		      		e.preventDefault();
+		      		window.location.href = "/update?role=student";
+		      	});
+		      	$(".teacher").click(function(e)
+		      	{
+		      		e.preventDefault();
+		      		window.location.href = "/update?role=teacher";
+		      	});
 		        if(!${loggedIn?c}) {
 		        	var lock = new Auth0Lock('${clientId}', '${clientDomain}', {
-		        		loginAfterSignup: false , 
+		        		loginAfterSignup: false ,
 			            auth: {
 			            	redirect : false ,
 			              params: {
-			                scope: 'openid user_id name nickname email picture'
+			                scope: 'openid user_id name nickname email'
 			              }
 			            }
 		        	});
