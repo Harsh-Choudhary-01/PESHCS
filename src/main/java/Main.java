d3932fea153d037a0435d8324f4cd3cbd561daa6
Harsh-Choudhary-01 <harshch000@gmail.com>
2016-12-28 20:04:44 -0600

    troubleshooting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -699,7 +699,7 @@ public class Main {
             BufferedWriter out = new BufferedWriter(new FileWriter(file));
             out.write(code);
             out.close();
-            ProcessBuilder pb = new ProcessBuilder("javac " + userID + "/Main.java");
+            ProcessBuilder pb = new ProcessBuilder("/app/.jdk/bin/javac " + userID + "/Main.java");
             pb.redirectErrorStream(true);
             Process compileProcess = pb.start();
             stdOutput = new BufferedReader(new InputStreamReader(compileProcess.getInputStream()));
@@ -714,7 +714,7 @@ public class Main {
                 return output;
             compileProcess.destroy();
             compileProcess.waitFor();
-            pb = new ProcessBuilder("java " + userID + "/Main");
+            pb = new ProcessBuilder("/app/.jdk/bin/javac/java " + userID + "/Main");
             pb.redirectErrorStream(true);
             Map<String , String> env = pb.environment();
             env.clear();
