6faa28e52828060099a68e92053816ff8a627e20
Harsh-Choudhary-01 <harshch000@gmail.com>
2017-01-03 21:40:02 -0600

    debugging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -853,7 +853,7 @@ public class Main {
             }
         }
         catch (Exception e) {
-            System.out.println("Error while receiving message: " + e);
+            e.printStackTrace();
         }
         finally {
             if(connection != null) try { connection.close(); } catch(SQLException e) {}
